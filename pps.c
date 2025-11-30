72e0fab52300378207809dd648ff3d7ff9bcc846
Aditya Sharma <dipanaditya0022@gmail.com>
2025-11-30 13:18:26 +0530

    Update pps.c

--- pps.c
+++ pps.c
@@ -1,4 +1,5 @@
 // Member 1(Aditya Sharma ) -Course Catalog and Data Structures
+
 catalog.h
     #ifndef CATALOG_H
     #define CATALOG_H
@@ -29,7 +30,54 @@ int findCourseIndex(char code[]);
     #include "catalog.h"
 Course catalog[MAX_COURSES];
 int courseCount = 0;
+
+// LOADS INITIAL CATALOG
+
+void loadCatalog(){
+    strcpy(catalog[0].code,"CS101");
+    catalog[0].creditHours = 3;
+    catalog[0].numPrereq = 0;
+    catalog[0].semester = 1;
+
+    strcpy(catalog[0].code,"CS102");
+    catalog[1].creditHours = 4;
+    catalog[1].numPrereq = 1;
+    strcpy(catalog[1].prereq[0],"CS101");
+    catalog[1].semster = 2;
+
+    strcpy(catalog[2].code,"CS201");
+    catalog[2].creditHours = 3;
+    catalog[2].numPrereq = 1;
+    strcpy(catalog[2].prereq[0],"CS102");
+    catalog[2].semester = 1;
+
+    courseCount = 3;
+}
+intfindCourseIndex(char code[]){
+    for(int i = 0; i<courseCount; i++){
+        if(strcmp(catalog[i].code,code) == 0)
+    } 
+    return -1;
+}
+void displayCatalog(){
+    printf("\n===== COURSE CATALOG =====\n");
+    for(int i = 0; i<courseCount; i++){
+        print(%s(%d credits) | Semester: %d | Prereq: ",
+            catalog[i].code,
+            catalog[i].creditHours,
+            catalog[i].semester);
+
+        if(catalog[i].numPrereq == 0) print("None");
+        else{
+            for(intj = 0; j<catalog[i].numprereq; j++)
+                printf("%s",catalog[i].prereq[j]);
+        }
+        printf("\n");
+    }    
+}
+
 // Member 4(Aditya Srivastav) - Schedule Checker(Time Conflicts + Overload) //
+
 #indef SCHEDULE_H
 #define SCHEDULE_H
 
