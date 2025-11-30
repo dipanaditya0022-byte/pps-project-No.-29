ac1039330a4bc2b371a04804d767483ca67f1268
Aditya Srivastava <25lbcs3129@culkomail.in>
2025-11-30 10:26:38 +0530

    Update pps.c

--- pps.c
+++ pps.c
@@ -18,3 +18,34 @@ typedef struct {
     char prereqs[MAX_PREREQ][MAX_CODE];
 } Course;
 
+// Member 4 - Schedule Checker(Time Conflicts + Overload) //
+#indef SCHEDULE_H
+#define SCHEDULE_H
+
+void checkScheduleConflicts();
+void checkOverload();
+
+#endif
+
+#include <stdio.h>
+#include "catalog.h"
+#include "planner.h"
+
+void checkScheduleConflicts(){
+    printf("\nChecking schedule conflicts...\n");
+    printf("(Demo) No time conflicts detected.\n");
+}
+
+void checkOverload(){
+    printf("\nChecking credit overload...\n");
+int total=0;
+for(int i=0;i<planCount;i++){
+int idx=findCourseindex(studentPlan[i]);
+total+=catalog[idx].creditHours;
+}
+
+if (total>18)
+    printf("WARNING: You are overloaded (%d credits)!\n",total);
+else
+    pritnf("Credit load OK (%d credits)\n",total);
+}
