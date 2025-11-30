f4c84bf89b83dc078294edd1c3298fc6a8951e03
Aditya Sharma <dipanaditya0022@gmail.com>
2025-11-30 18:12:02 +0530

    Update pps.c

--- pps.c
+++ pps.c
@@ -84,10 +84,9 @@ prerequisites.c
 #include "catalog.h"
 #include "prerequisites.h"
 
-// MEMBER 2 CONTRIBUTION: DFS cycle detection for prerequisites
-// MEMBER 2 GAURANSH RAI 
-int visited[MAX_COURSES], recStack[MAX_COURSES];
 
+// MEMBER 2(GAURANSH RAI) CONTRIBUTION: DFS cycle detection for prerequisites
+int visited[MAX_COURSES], recStack[MAX_COURSES];
 int dfs(int v) {
     visited[v] = 1;
     recStack[v] = 1;
@@ -135,8 +134,58 @@ int checkPrerequisites(char courseCode[]) {
     return1 ;
 }
 
-// Member 4(Aditya Srivastava) - Schedule Checker(Time Conflicts + Overload) //
+planner.h
+#ifndef PLANNER_H
+#define PLANNER_H
 
+void addCourseToPlan();
+void displayPlan();
+
+#endif
+
+planner.c
+#include <stdio.h>
+#include <string.h"
+#include "catalog.h"
+#include "prerequisites.h"
+#include "planner.h"
+
+
+// MEMBER 3(KASHISH SINGH)CONTRIBUTION: Maintain student's semester plan
+char studentPlan[20][10];
+int planCount = 0;
+
+void addCourseToPlan() {
+    char code[10];
+    printf("Enter course code to add: ");
+    scanf("%s", code);
+
+    int idx = findCourseIndex(code);
+    if (idx == -1) {
+        printf("Course not found!\n");
+        return;
+    }
+
+    checkPrerequisites(code);
+    strcpy(studentPlan[planCount++], code);
+    printf("Added %s to your plan.\n", code);
+}
+
+void displayPlan() {
+    printf("\n===== STUDENT PLAN =====\n");
+    int credits = 0;
+
+    for (int i = 0; i < planCount; i++) {
+        int idx = findCourseIndex(studentPlan[i]);
+        printf("%s (%d credits)\n", studentPlan[i], catalog[idx].creditHours);
+        credits += catalog[idx].creditHours;
+    }
+
+    printf("Total Credits: %d\n", credits);
+}
+
+
+// Member 4(Aditya Srivastava) - Schedule Checker(Time Conflicts + Overload) //
 void checkScheduleConflicts(){
     printf("\nChecking schedule conflicts...\n");
     printf("(Demo) No time conflicts detected.\n");
